084d2742cd91f8b0de2d1a915dff29390f9814b0
Nikhat Nadaf <nikhatnadaf1128@gmail.com>
2024-06-09 09:32:40 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        mycolor:"#390e6e",
+        org:"#edbf74"
+      },
+    },
+  },
+  plugins: [],
+}
+
